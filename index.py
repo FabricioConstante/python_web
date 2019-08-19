b776d31100bbdb28be1ec8e11a7cd5cb1db506fa
FabricioConstante <35309597+FabricioConstante@users.noreply.github.com>
2019-08-19 08:58:17 -0500

    actualizado

--- index.py
+++ index.py
@@ -1,7 +1,39 @@
+import os
+import glob, os.path
 import pandas as pd
-from flask import Flask, render_template, redirect, url_for, request
+# import string
+from flask import *
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['xlsx'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload_file', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file',
+                                    filename=filename))
+    return render_template('upload_file.html')
 
 @app.route('/')
 def home():
@@ -13,11 +45,26 @@ def about():
 
 @app.route('/matriz')
 def matriz():
-    # prueba fallida de subida del documento excel
-    # xlsx_file=pd.ExcelFile('C:\\Users\\Adminpc\\Desktop\\pyton_web\\uploads\\27001.xlsx')
-    # return xlsx_file.to_html()
-    df = pd.read_excel( "C:\\Users\\Adminpc\\Desktop\\pyton_web\\uploads\\27001.xlsx" )
-    return df.to_html()
+    return render_template('matriz.html')
+
+@app.route('/llamar_matriz')
+def llamar_matriz():
+    documento = "uploads/data.xlsx"
+    ingresar_documento = pd.ExcelFile( documento )
+    hojas = []
+    for valor in ingresar_documento.sheet_names:
+        hojas.append (valor)
+            # for i 
+        print(valor)
+    print(len(hojas))
+    print(ingresar_documento.sheet_names)
+    lectura_del_documento = pd.read_excel( ingresar_documento , index_col=0, header=1,sheet_name='Personal')
+    return lectura_del_documento.to_html()
+
+# @app.route('/dropdown', methods=['GET'])
+# def dropdown():
+#     colours = ['Red', 'Blue', 'Black', 'Orange']
+#     return render_template('test.html', colours=colours)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
